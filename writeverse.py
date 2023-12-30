82089277e7052ed13ef4cf5a0408d56821a9b3e6
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2023-12-30 16:49:07 +0545

    Introduce layout, add navigation bar and add few styles

--- writeverse.py
+++ writeverse.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Tangsang Chongbang',
+        'title': 'Blog Post 1',
+        'content': 'First posts content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'John Cena',
+        'title': 'Blog Post 2',
+        'content' : 'Second posts content',
+        'date_posted': 'April 23, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == '__main__':
