3c0d649bb6420e58a8bc5807037833940debe46e
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-01-01 08:24:29 +0545

    Complete Login Functionality

--- writeverse.py
+++ writeverse.py
@@ -22,7 +22,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template("home.html", posts=posts)
 
 
@@ -36,14 +36,20 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account Created For {form.username.data}!', 'success')
-        return redirect("/")
+        return redirect(url_for("home"))
 
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful! Please check your email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
