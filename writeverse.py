7e86ab119739f1cb1c881b8281dcea1881c0a9d7
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-01-03 20:53:38 +0545

    Create Database and Fix DB bugs

--- writeverse.py
+++ writeverse.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -21,12 +21,12 @@ class User(db.model):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post'{self.title}', '{self.date_posted}'"
