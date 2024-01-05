98f71f42951184c0ee47d5db623179bdbdfac455
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-01-05 08:19:22 +0545

    Add Registration Validation

--- writeverse/__init__.py
+++ writeverse/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '154ba97b464eaa4546579d0d0c7648d1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from writeverse import routes
--- writeverse/routes.py
+++ writeverse/routes.py
@@ -1,5 +1,5 @@
 from writeverse.models import User, Post
-from writeverse import app
+from writeverse import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect
 from writeverse.forms import RegistrationForm, LoginForm
 
@@ -34,8 +34,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account Created For {form.username.data}!', 'success')
-        return redirect(url_for("home"))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You can login now!', 'success')
+        return redirect(url_for("login"))
 
     return render_template('register.html', title='Register', form=form)
 
