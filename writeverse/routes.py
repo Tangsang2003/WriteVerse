8a9518277034dfb5acfb889980706436bd5e3e07
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-01-06 21:09:56 +0545

    Add profile update feature

--- writeverse/routes.py
+++ writeverse/routes.py
@@ -1,7 +1,7 @@
 from writeverse.models import User, Post
 from writeverse import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request
-from writeverse.forms import RegistrationForm, LoginForm
+from writeverse.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
@@ -33,7 +33,7 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -72,7 +72,18 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template("account.html", title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template("account.html", title='Account', image_file=image_file, form=form)
