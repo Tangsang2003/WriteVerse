da960fab2cc0b872e0ad65a1f9dcde0d69255217
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-01-07 08:32:39 +0545

    Add Update Profile Picture Feature

--- writeverse/routes.py
+++ writeverse/routes.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+from PIL import Image
 from writeverse.models import User, Post
 from writeverse import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request
@@ -72,11 +75,26 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (250, 250)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
